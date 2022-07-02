be886927c3d3448c9a246a3f69ed2ddabc400bb3
Sofia Lacosta <sofi.lacosta@hotmail.com>
2022-07-02 14:29:31 -0300

    primerEntrega

---
+++ index.js
@@ -0,0 +1,100 @@
+let numeroGanador = 5;
+let numero = parseInt(prompt("Ingrese un numero del 1 al 10 para descubrir su descuento!"));
+let total_pagar = 0;
+let descuento = 0;
+numerosCercanos = 4 , 6;
+numeroLejanos = 1, 2 , 3 , 7 , 8 , 9 , 10;
+
+if (numero === numeroGanador) {
+    alert("ACERTASTE!!! Tenes 25% de descuento en toda la tienda! ");
+    descuento = 25;
+} else if (numero === numerosCercanos) {
+    alert("No es el numero ganador! pero estuviste muy cerca! Tenes 15% de descuento en la tienda! ");
+    descuento = 15;
+} else if (numero === numeroLejanos) {
+    alert("No es el numero ganador! Te llevas 10% de descuento por participar");
+    descuento = 10;
+} else {
+    alert("No es un numero correcto");
+    descuento = 0;
+}
+
+console.log(descuento)
+
+let cantBordados = parseInt(prompt("Ingrese la cantidad de Bordados:"));
+let precioDelBordadoGrande = 400;
+let precioDelBordadoChico = 300;
+
+
+for (let i = 0; i < cantBordados; i++) {
+    let numeroDeBordado = 1 + i;
+    let tamañoDelBordado = parseInt(prompt("Ingrese el tamaño (1: si es chico o 2: si es grande) del bordado  n°" + numeroDeBordado));
+    if (tamañoDelBordado == 1) {
+        alert("Agregaste un Bordado chico por $300");
+        total_pagar = total_pagar + precioDelBordadoChico
+        continue;
+    } else if (tamañoDelBordado == 2) {
+        alert("Agregaste un Bordado Grande por $400");
+        total_pagar = total_pagar + precioDelBordadoGrande
+        continue;
+    } else {
+        alert("El tamaño no es correcto");
+        continue;
+    }
+}
+
+descuento = (total_pagar * descuento) / 100
+
+let mensaje_final = "El total a pagar con tu descuento es: $" + (total_pagar - descuento);
+alert(mensaje_final);
+console.log(mensaje_final);
+
+
+/* while (true) {
+    switch (numero) {
+        case 1:
+            alert("1 no es el numero ganador! Te llevas 10% de descuento por participar");
+            descuento = total_pagar * 0.10;
+            
+        case 2:
+            alert("2 no es el numero ganador! Te llevas 10% de descuento por participar");
+            descuento = total_pagar * 0.10;
+            repetir = 0;
+        case 3:
+            alert("3 no es el numero ganador! Te llevas 10% de descuento por participar");
+            descuento = total_pagar * 0.10;
+            repetir = 0;
+        case 4:
+            alert("4 no es el numero ganador! pero estuviste muy cerca! Tenes 15% de descuento en la tienda! ");
+            descuento = total_pagar * 0.15;
+            repetir = 0;
+        case 5:
+            alert("ACERTASTE!!! Tenes 25% de descuento en toda la tienda! ");
+            descuento = total_pagar * 0.25;
+            repetir = 0;
+        case 6:
+            alert("6 no es el numero ganador! pero estuviste muy cerca! Tenes 15% de descuento en la tienda! ");
+            descuento = total_pagar * 0.15;
+            repetir = 0;
+        case 7:
+            alert("7 no es el numero ganador! Te llevas 10% de descuento por participar");
+            descuento = total_pagar * 0.10;
+            repetir = 0;
+        case 8:
+            alert("8 no es el numero ganador! Te llevas 10% de descuento por participar");
+            descuento = total_pagar * 0.10;
+            repetir = 0;
+        case 9:
+            alert("9 no es el numero ganador! Te llevas 10% de descuento por participar");
+            descuento = total_pagar * 0.10;
+            repetir = 0;
+        case 10:
+            alert("10 no es el numero ganador! Te llevas 10% de descuento por participar");
+            descuento = total_pagar * 0.10;
+            repetir = 0;
+        default:
+            alert("No es un numero correcto");
+            descuento = 0;
+            repetir = 0;
+    }
+} */
