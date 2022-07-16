4f1304c0a9f3629164fd39633d21f3d027470e07
Sofia Lacosta <sofi.lacosta@hotmail.com>
2022-07-16 12:09:33 -0300

    htmlyJs

--- iniciarSesion.js
+++ iniciarSesion.js
@@ -1,20 +1,23 @@
-/* let nombreDeUsuario = prompt("Ingrese un nombre de usuario").toLowerCase();
+let nombreDeUsuario = prompt("Ingrese un nombre de usuario").toLowerCase();
 let contraseña = prompt("Ingrese una contraseña").toLowerCase();
 alert("Registro completado, inicie secion para continuar")
-console.log(nombreDeUsuario, contraseña) */
+console.log(nombreDeUsuario, contraseña)
 
-/* let formulario = document.getElementById("formulario");
+let formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
     let info = e.target;
-    if(info.children[2].value === nombreDeUsuario && info.children[5].value===contraseña){
-        alert("formulario enviado");
-        
+    if(info.children[0].value === nombreDeUsuario && info.children[1].value===contraseña){
+        let info = document.createElement("h5");
+        info.className = "informacion";
+        info.innerHTML = "Iniciando Sesion"
+        document.body.append(info)
     }else{
         let info = document.createElement("h5");
         info.className = "rojo";
-        info.innerHTML = "Correo erroneo"
-        
+        info.innerHTML = "Usuario o contraseña incorectos"
+        document.body.append(info);
     }  
-    }); */
+    });
     
 
